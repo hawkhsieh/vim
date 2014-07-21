75062dc9c688b44d42edb2bb652de7fd22fb0d87
hawk_hsieh <hawk_hsieh@miiicasa.com>
2014-07-21 14:30:06 +0800

    install cmake and phthon before plugin installing

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 cp -a .vim ~
 cp .vimrc ~
+
+sudo apt-get -y install cmake
+sudo apt-get -y install python-dev
+
+cd ~/.vim
+./install.sh --clang-completer
+
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim +PluginInstall +qall
 cd ~/.vim/bundle/YouCompleteMe
