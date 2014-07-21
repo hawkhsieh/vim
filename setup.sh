03e21d33db916243da813708f00173ba533bfc0c
hawk_hsieh <hawk_hsieh@miiicasa.com>
2014-07-21 15:07:07 +0800

    The YCM has to install.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,17 @@
+#----      Caution !!!    ------------
+# - The go version has to be 1.3
+# - The GOROOT has to be /usr/local/go
+# - The GOPATH has not to be empty
+
 cp -a .vim ~
 cp .vimrc ~
 
 sudo apt-get -y install cmake
 sudo apt-get -y install python-dev
 
-cd ~/.vim
-./install.sh --clang-completer
 
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim +PluginInstall +qall
 cd ~/.vim/bundle/YouCompleteMe
 git submodule update --init --recursive
+./install.sh --clang-completer
